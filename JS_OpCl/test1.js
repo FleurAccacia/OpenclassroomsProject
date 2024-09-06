03149f2577867a8e7e124a576a8a18a8e8216f56
Gracia GOKAR <graciajulienne@gmail.com>
2024-09-06 18:30:25 +0000

    premier test

---
+++ JS_OpCl/test1.js
@@ -0,0 +1,11 @@
+let totalLivres = 500
+console.log("Le nombre total de livres initials est de " ,totalLivres)
+
+totalLivres+=50
+console.log (totalLivres)
+
+totalLivres-=10
+console.log(totalLivres)
+
+affichageTotalLivres=totalLivres+5
+console.log ("Notre bibliotheque possede ", affichageTotalLivres,"  livres")
