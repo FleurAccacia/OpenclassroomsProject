a70f459a84ed2d2afd9a5214d6fc2a6151316079
Gracia GOKAR <graciajulienne@gmail.com>
2024-09-06 18:58:41 +0000

    tableau en js

---
+++ JS_OpCl/tableau.js
@@ -0,0 +1,13 @@
+const playlist = ["Ommema","Come jesus come","All the matters"]
+
+//let totalMorceaux = playlist.length
+//console.log (totalMorceaux)
+
+playlist.push("Holy spirit","De gloire en gloire")
+
+// console.log (playlist)
+// let totalMorceaux = playlist.length
+// console.log (totalMorceaux)
+
+playlist.pop(playlist[4])
+console.log (playlist)
