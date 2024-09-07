7c246afb3ddbcbc5497829c5fe04b45aa1c4a946
Gracia GOKAR <graciajulienne@gmail.com>
2024-09-07 06:53:43 +0000

    main

---
+++ main.js
@@ -0,0 +1,20 @@
+// var score = 10;
+// console.log('Le "score" est de : ' + score);
+// score = score + 10;
+// console.log('Le "score" est de : ' + score);
+// score += 10;
+// score += 10;
+// score += 10;
+// console.log('Le "score" est de : ' + score);
+// score *= 3;
+// console.log('Le "nouveau score" est de : ' + score);
+
+var prenom = "Gracia"
+var nom ="GOKAR"
+console.log("**********avant la permutation**************")
+console.log("Prénom:"+prenom)
+console.log("Nom:"+nom)
+
+console.log("**********après la permutation**************")
+console.log("Prénom:"+nom)
+console.log("Nom:"+prenom)
